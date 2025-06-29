bd9c0cf2026a51fbe1febbd243dcf652fd0f54c4
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2025-06-29 09:53:29 +0530

    fixed skill page max width

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <main className='bg-black/95 min-h-screen'>
+        <main className=' min-h-screen'>
             <p className='text-3xl text-gray-400 text-center border border-red-300'>this is contct & last page</p>
         </main>
     )
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -26,46 +26,48 @@ import { Cover } from '../components/ui/cover';
 
 
 const Skills =()=>{
-
+//  <section className="border border-b-green-800 min-h-screen px-4  mx-auto bg-gradient-to-l from-black/80 via-black/85 to-black/95 text-white" id="skills"></section>
     return (
-    <section className="py-10 min-h-screen px-4  mx-auto bg-gradient-to-l from-black/80 via-black/85 to-black/95 text-white" id="skills">
-        <div className="text-center mb-12">
-            {/* <h2 className="text-4xl font-bold text-gray-400 mb-4">Technical Skills</h2> */}
-            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold max-w-7xl mx-auto text-center py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
-                <Cover>Tech Stack</Cover>
-            </h1>
-            <p className="text-lg text-gray-600 max-w-4xl mx-auto">
-                A comprehensive overview of my technical expertise across Frontend and Backend Technologies.
-            </p>
-		</div>
+    <section className="min-h-screen px-4 text-white" id="skills">
+        <main className="max-w-6xl mx-auto">
+            <nav className="text-center mb-12">
+                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold max-w-7xl mx-auto text-center py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
+                    <Cover>Tech Stack</Cover>
+                </h1>
+                <p className="text-lg text-gray-600 max-w-4xl mx-auto">
+                    A comprehensive overview of my technical expertise across Frontend and Backend Technologies.
+                </p>
+		    </nav>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-            {categories.map((category) => (
-            <div key={category.title} className="bg-gray-800 rounded-lg p-6">
-                <h3 className="text-xl font-semibold mb-4 text-center">{category.title}</h3>
-                <div className="flex flex-wrap justify-center gap-4">
-                {category.skills.map((skill) => (
-                    <div key={skill} className="relative group">        
-                    <img
-                       src={
-                            skill == "graphql"
-                            ? "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/graphql/graphql-plain.svg"
-                            : skill == "recoil" ? recoil 
-                            : skill =="jwt" ? jwt 
-                            :`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${skill}/${skill}-original.svg`
-                        }
-                        alt={skill}
-                        className="w-14 h-14 transition-transform duration-300 group-hover:scale-110"
-                    />
-                    <span className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-full text-xs opacity-0 group-hover:opacity-100 mt-2 bg-gray-800 px-2 py-1 rounded">
-                        {skill.charAt(0).toUpperCase() + skill.slice(1)}
-                    </span>
+            <nav className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+                {categories.map((category) => (
+                <div key={category.title} className="bg-gray-800 rounded-lg p-6">
+                    <h3 className="text-xl font-semibold mb-4 text-center">{category.title}</h3>
+                    <div className="flex flex-wrap justify-center gap-4">
+                    {category.skills.map((skill) => (
+                        <div key={skill} className="relative group">        
+                            <img
+                            src={
+                                    skill == "graphql"
+                                    ? "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/graphql/graphql-plain.svg"
+                                    : skill == "recoil" ? recoil 
+                                    : skill =="jwt" ? jwt 
+                                    :`https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${skill}/${skill}-original.svg`
+                                }
+                                alt={skill}
+                                className="w-14 h-14 transition-transform duration-300 group-hover:scale-110"
+                            />
+                            <span className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-full text-xs opacity-0 group-hover:opacity-100 mt-2 bg-gray-800 px-2 py-1 rounded">
+                                {skill.charAt(0).toUpperCase() + skill.slice(1)}
+                            </span>
+                        </div>
+                    ))}
                     </div>
-                ))}
                 </div>
-            </div>
-            ))}
-        </div>
+                ))}
+            </nav>
+        </main>
+       
     </section>
   )
 
