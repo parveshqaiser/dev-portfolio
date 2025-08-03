9001d09025396bde38016b2503e2a93a5e2391b7
Parvesh Qaiser <ahmed75parvesh@gmail.com>
2025-08-03 08:50:57 +0530

    added fav icon & removed git from nab bar

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -44,13 +44,13 @@ const Navbar =()=> {
             ),
             href: "#experience",
         },
-        {
-            title: "GitHub",
-            icon: (
-                <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
-            ),
-            href: "#",
-        },
+        // {
+        //     title: "GitHub",
+        //     icon: (
+        //         <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+        //     ),
+        //     href: "#",
+        // },
         {
             title: "Contact",
             icon: (
